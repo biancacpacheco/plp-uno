25efa8695ef16fdc2c27064c1ee1c5bec23f19e2
Bianca Pacheco <bianca.pacheco@ccc.ufcg.edu.br>
2022-11-10 21:28:58 -0300

    conserta strings de impressão

--- uno.hs
+++ uno.hs
@@ -1,9 +1,9 @@
-//menu de start!!
+--menu de start!!
 main:: IO()
 main = do
-    print("BEM-VINDO, JOGADOR! :)")
-    print("")
-    print("escolha uma opção:")
-    print("(1) iniciar partida")
-    print("(2) hstórico de pontuação")
-    print("(3) regras do jogo")
+    putStrLn("BEM-VINDO, JOGADOR! :)")
+    putStrLn("")
+    putStrLn("escolha uma opção:")
+    putStrLn("(1) iniciar partida")
+    putStrLn("(2) histórico de pontuação")
+    putStrLn("(3) regras do jogo")
