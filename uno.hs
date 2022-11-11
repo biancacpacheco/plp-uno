4644dea8e72dc919b1695823b1c1ce60cbde8160
Bianca Pachêco <86495912+biancacpacheco@users.noreply.github.com>
2022-11-10 21:33:10 -0300

    Update uno.hs

--- uno.hs
+++ uno.hs
@@ -1,18 +1,10 @@
 --menu de start!!
 main:: IO()
 main = do
-<<<<<<< HEAD
     putStrLn("BEM-VINDO, JOGADOR! :)")
     putStrLn("")
     putStrLn("escolha uma opção:")
     putStrLn("(1) iniciar partida")
     putStrLn("(2) histórico de pontuação")
     putStrLn("(3) regras do jogo")
-=======
-    print("BEM-VINDO, JOGADOR! :)")
-    print("")
-    print("escolha uma opção:")
-    print("(1) iniciar partida")
-    print("(2) histórico de pontuação")
-    print("(3) regras do jogo")
->>>>>>> b67353afef34af1495d2213c6082ff9a8279f1f0
+
