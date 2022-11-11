5feee6dae09b5073c9f328c1d36db38ddd1e6f80
Bianca Pacheco <bianca.pacheco@ccc.ufcg.edu.br>
2022-11-10 21:09:39 -0300

    primeiros prints

---
+++ uno.hs
@@ -0,0 +1,9 @@
+//menu de start!!
+main:: IO()
+main = do
+    print("BEM-VINDO, JOGADOR! :)")
+    print("")
+    print("escolha uma opção:")
+    print("(1) iniciar partida")
+    print("(2) hstórico de pontuação")
+    print("(3) regras do jogo")
