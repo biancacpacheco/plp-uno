d2e4b97ee2982115d2bf58ad8c2ec99941fd0a95
Bianca Pachêco <86495912+biancacpacheco@users.noreply.github.com>
2022-11-11 18:57:53 -0300

    Update uno.hs

--- uno.hs
+++ uno.hs
@@ -12,7 +12,6 @@ menuStart = do
     putStrLn("(1) iniciar partida")
     putStrLn("(2) histórico de pontuação")
     putStrLn("(3) regras do jogo")
-<<<<<<< HEAD
     putStrLn("(4) sair do jogo")
     opcao <- getLine
     verificaOpcao (read opcao)
@@ -25,6 +24,4 @@ verificaOpcao opcao =
     else do         
         putStrLn("Por favor, escolha uma opção válida") 
         menuStart
-=======
 
->>>>>>> 71937acd921604c926e8379d6a91ec50b8b0bc2b
