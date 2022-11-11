40827e85fd9de68713ce1dfa882a445219ff0781
Bianca Pacheco <bianca.pacheco@ccc.ufcg.edu.br>
2022-11-11 18:54:50 -0300

    valida opcao

--- uno.hs
+++ uno.hs
@@ -1,18 +1,26 @@
 --menu de start!!
 main:: IO()
 main = do
-<<<<<<< HEAD
     putStrLn("BEM-VINDO, JOGADOR! :)")
     putStrLn("")
+    menuStart
+
+
+menuStart :: IO()
+menuStart = do
     putStrLn("escolha uma opção:")
     putStrLn("(1) iniciar partida")
     putStrLn("(2) histórico de pontuação")
     putStrLn("(3) regras do jogo")
-=======
-    print("BEM-VINDO, JOGADOR! :)")
-    print("")
-    print("escolha uma opção:")
-    print("(1) iniciar partida")
-    print("(2) histórico de pontuação")
-    print("(3) regras do jogo")
->>>>>>> b67353afef34af1495d2213c6082ff9a8279f1f0
+    putStrLn("(4) sair do jogo")
+    opcao <- getLine
+    verificaOpcao (read opcao)
+
+
+verificaOpcao :: Int -> IO()
+verificaOpcao opcao =
+    if (elem opcao [1,2,3,4]) then
+        putStrLn("direcionando...")
+    else do         
+        putStrLn("Por favor, escolha uma opção válida") 
+        menuStart
